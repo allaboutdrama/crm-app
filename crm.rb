7cd5da12fe2c721a4040a62d068799da646f75a9
allaboutdrama <allaboutdramaproductions@gmail.com>
2014-04-01 20:05:12 -0400

    Created new routes and its placeholders

--- crm.rb
+++ crm.rb
@@ -1,10 +1,26 @@
 require 'sinatra'
 
-
-
-
 get '/' do 
   @crm_app_name = "KUNDEN"
   @current_time = Time.now
   erb :index
 end  
+
+
+get '/new' do 
+  erb :new
+ end  
+
+get '/id' do
+  erb :id
+end
+
+get '/customers' do
+  erb :customers
+end  
+
+get '/modify' do
+  erb :modify
+end
+
+
